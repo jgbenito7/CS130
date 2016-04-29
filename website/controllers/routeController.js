8b4f92e37cf5d963dfbd347c5c23f7558261d208
Joey Gomez-Benito <jgbenito7@gmail.com>
2016-04-29 12:45:47 -0700

    Basic upload done

--- website/controllers/routeController.js
+++ website/controllers/routeController.js
@@ -16,18 +16,24 @@ rescueApp.config(['$routeProvider',
       });
   }]);
 
-rescueApp.factory('Animals', ['$resource',
-  function($resource){
-    return $resource('http://localhost:8080/test2', {}, {
-      query: {method:'GET'}
-    });
-  }]);
-
-rescueApp.controller('testCtrl', function($scope,Animals) {
+rescueApp.controller('testCtrl', function($scope,$http) {
         // create a message to display in our view
         $scope.message = 'Everyone come and see how good I look!';
 
+        var dataObj = {
+				      animal_type : "Seal",
+				      animal_notes : "Testing, testing, 123.",
+		    };
+  		  $http.post('http://54.186.47.42/reports', dataObj).success(function(data) {
+			       alert("Success");
+		    });
 
-        $scope.animals = Animals.query();
-        console.log($scope.animals);
+
+      //  $scope.animals = Animals.query();
+        //console.log($scope.animals);
+
+        /*$http.get('http://54.186.47.42/reports').success(function(data) {
+          $scope.animals = data;
+          console.log($scope.animals);
+        });*/
 });
