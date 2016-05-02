db8798aee5ed488e03c29dccae5b0fe1d16b8fe1
Joey Gomez-Benito <joeygomez-benito@Joeys-Mac-Pro.local>
2016-05-02 15:42:27 -0700

    Basic form working

--- website/controllers/routeController.js
+++ website/controllers/routeController.js
@@ -18,27 +18,49 @@ rescueApp.config(['$routeProvider',
 
 rescueApp.controller('homeCtrl', function($scope,$http) {
         // create a message to display in our view
-        $scope.data = {};
+        $scope.dataObj = {};
+        $scope.latitude;
+        $scope.longitude;
 
         $scope.getData = function(form){
-          $scope.data = angular.copy(form);
-          console.log($scope.data);
+          $scope.dataObj = angular.copy(form);
+          $scope.dataObj.latitude = $scope.latitude;
+          $scope.dataObj.longitude = $scope.longitude;
+          console.log($scope.dataObj);
+
+          /*$http.post('http://54.186.47.42/reports', $scope.dataObj).success(function(data) {
+               console.log("Success");
+          });*/
         }
 
-        /*var dataObj = {
-				      animal_type : "Seal",
-				      animal_notes : "Testing, testing, 123.",
-		    };
-  		  $http.post('http://54.186.47.42/reports', dataObj).success(function(data) {
-			       alert("Success");
-		    });*/
+        $scope.location = function(){
+              if (navigator.geolocation) {
+                  navigator.geolocation.getCurrentPosition(function(position){
+                    $scope.latitude = position.coords.latitude;
+                    $scope.longitude = position.coords.longitude;
+                  });
+              } else {
+                  $scope.message = "Geolocation is not supported by this browser.";
+              }
 
 
-      //  $scope.animals = Animals.query();
-        //console.log($scope.animals);
+        }
 
-        /*$http.get('http://54.186.47.42/reports').success(function(data) {
-          $scope.animals = data;
-          console.log($scope.animals);
-        });*/
+
+        $scope.uploadFile = function(event){
+          var files = event.target.files;
+          $scope.dataObj.file_name = files[0].name;
+        };
+
+
+});
+
+rescueApp.directive('customOnChange', function() {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      var onChangeHandler = scope.$eval(attrs.customOnChange);
+      element.bind('change', onChangeHandler);
+    }
+  };
 });
