a4b56ad188f7b5a9ed4c39864fb4dd4df00fd683
Jacob Moghtader <jake@moghtader.co>
2016-05-04 21:01:30 -0700

    file upload maybe

--- server/test.js
+++ server/test.js
@@ -1,6 +1,7 @@
 var mysql      = require('mysql');
 var restify = require('restify');
 var sanitizer = require('sanitizer');
+var fs = require('fs');
 
 var connection  = mysql.createPool({
   connectionLimit : 10,
@@ -11,16 +12,8 @@ var connection  = mysql.createPool({
   port  : 3306
 });
 
-
-function test1(req, res, next) {
-  console.log(req.params.amount);
-  res.send("Hello 1");
-  next();
-}
-
 // Insert animal into Reports table
 function createReport (req, res, next) {
-  console.log(req);
   connection.query("INSERT INTO Reports (type, notes, longitude, latitude) VALUES("+
     mysql.escape(req.body.animal_type)+
     "," + mysql.escape(req.body.animal_notes) + 
@@ -94,6 +87,51 @@ function authorizeUser (req, res, next) {
 
 }
 
+function testFileUpload(req, res, next) {
+  var filenames = [];
+  var counter = 0;
+  for(i in req.files) {
+    var re = /(?:\.([^.]+))?$/;
+    var ext = re.exec(req.files[i].name);
+    if(ext == undefined)
+      continue; //no file extension should throw an alarm
+    var filename = (new Date).getTime() + counter + "." + ext[1];
+    counter++;
+    filenames.push(filename);
+    fs.createReadStream(req.files[i].path).pipe(fs.createWriteStream("images/" + filename));
+  }
+  console.log(filenames); //insert into 
+  connection.query("INSERT INTO Reports (type, notes, longitude, latitude) VALUES("+
+    mysql.escape(req.body.animal_type)+
+    "," + mysql.escape(req.body.animal_notes) + 
+    "," + mysql.escape(parseFloat(req.body.longitude)) +
+    ","+mysql.escape(parseFloat(req.body.latitude)) + ");", function(err, results) {
+      if(err) 
+        throw err;
+      if(filenames.length > 0) {
+        connection.query("SET @lastid = LAST_INSERT_ID();", function(err, results) {
+          if(err)
+            throw err;
+          var queryString = "INSERT INTO filename (report_id, filename) VALUES ";
+          for(var i = 0; i < filenames.length; i++) {
+            queryString += " (@lastid, " + filesnames[i] + ")";
+            if(i != filesnames.length - 1) {
+              queryString += ", ";
+            } else {
+              queryString += ";"
+            }
+          }
+
+          connection.query(queryString, function(err, results) {
+            if(err)
+              throw err;
+          });
+        });
+    }
+  });
+  res.send(200);
+  next();
+}
 
 var server = restify.createServer();
 server.use(function crossOrigin(req,res,next){
@@ -102,15 +140,19 @@ server.use(function crossOrigin(req,res,next){
     return next();
   });
 
-server.use(restify.bodyParser ({mapParams: false}));
+server.use(restify.bodyParser ({mapParams: false,
+    multiples: true}));
 
 //server.get('/reports/create/:animal_type/:animal_notes', createReport);
 //server.get('/users/create/:email/:org_id/:password', createUser);
 server.post('/reports', createReport);
 server.post('/users', createUser);
 server.get('/reports', getReports);
-server.post('/users/authorize', authorizeUser)
-
+server.post('/users/authorize', authorizeUser);
+server.post('/testfile', testFileUpload);
+server.get(/\/images\/?.*/, restify.serveStatic({
+    directory: __dirname
+}));
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
