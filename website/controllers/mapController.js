6ae2b11d7c4e1441ee736346225b2b197cd7e6cc
Joey Gomez-Benito <jgbenito7@gmail.com>
2016-05-10 00:51:38 -0700

    Changed initialize trigger

--- website/controllers/mapController.js
+++ website/controllers/mapController.js
@@ -1,6 +1,8 @@
 rescueApp.controller('mapCtrl', function($scope,$http) {
 
   $http.get('https://www.rescuehero.org/reports').success(function(data) {
+
+    console.log("Request made");
        function initialize() {
          var map = {
            center:new google.maps.LatLng(34.4707,-119.8311),
@@ -12,17 +14,10 @@ rescueApp.controller('mapCtrl', function($scope,$http) {
          //var myLatLng = {lat: 34.4707, lng: -119.8311};
          var myLatLng = new google.maps.LatLng(34.4707,-119.8311);
 
-         console.log(myLatLng);
-
-         /*var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: 'Hello World!'
-        });*/
-          var marker;
+         var marker;
          for (var i=0; i<data.length; i++){
            var posObj = new google.maps.LatLng(parseFloat(data[i]["latitude"]),parseFloat(data[i]["longitude"]));
-           console.log(posObj);
+           //console.log(posObj);
            marker = new google.maps.Marker({
              position: posObj,
              map: map,
@@ -30,7 +25,7 @@ rescueApp.controller('mapCtrl', function($scope,$http) {
            });
          }
        }
-       google.maps.event.addDomListener(window, 'load', initialize);
+       initialize();
        //$scope.$apply();
   });
 
